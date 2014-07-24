468cf64b8ffcb726e5b01d8612390d48cdf95501
Fahd Sultan <fahd@simulmedia.com>
2014-07-23 23:01:41 -0400

    added fedora urls

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['yum']['postgresql']['version'] = "9.2"
 default['yum']['postgresql']['arch'] = "x86_64"
 
-default['yum']['postgresql']['9.3']['centos']['5']['x86_64']['url'] = 'http://yum.postgresql.org/9.4/redhat/rhel-5-x86_64/pgdg-centos94-9.4-1.noarch.rpm'
+default['yum']['postgresql']['9.4']['centos']['5']['x86_64']['url'] = 'http://yum.postgresql.org/9.4/redhat/rhel-5-x86_64/pgdg-centos94-9.4-1.noarch.rpm'
 default['yum']['postgresql']['9.4']['centos']['5']['i386']['url'] = 'http://yum.postgresql.org/9.4/redhat/rhel-5-x86_64/pgdg-centos94-9.4-1.noarch.rpm'
 default['yum']['postgresql']['9.4']['centos']['6']['x86_64']['url'] = 'http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm'
 default['yum']['postgresql']['9.4']['centos']['6']['i386']['url'] = 'http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm'
@@ -15,3 +15,20 @@ default['yum']['postgresql']['9.2']['centos']['5']['x86_64']['url'] = 'http://yu
 default['yum']['postgresql']['9.2']['centos']['5']['i386']['url'] = 'http://yum.postgresql.org/9.2/redhat/rhel-5-x86_64/pgdg-centos92-9.2-6.noarch.rpm'
 default['yum']['postgresql']['9.2']['centos']['6']['x86_64']['url'] = 'http://yum.postgresql.org/9.2/redhat/rhel-6-x86_64/pgdg-centos92-9.2-6.noarch.rpm'
 default['yum']['postgresql']['9.2']['centos']['6']['i386']['url'] = 'http://yum.postgresql.org/9.2/redhat/rhel-6-x86_64/pgdg-centos92-9.2-6.noarch.rpm'
+
+default['yum']['postgresql']['9.4']['fedora']['19']['x86_64']['url'] = 'http://yum.postgresql.org/9.4/fedora/fedora-19-x86_64/pgdg-fedora94-9.4-1.noarch.rpm'
+default['yum']['postgresql']['9.4']['fedora']['19']['i386']['url'] = 'http://yum.postgresql.org/9.4/fedora/fedora-19-x86_64/pgdg-fedora94-9.4-1.noarch.rpm'
+default['yum']['postgresql']['9.4']['fedora']['20']['x86_64']['url'] = 'http://yum.postgresql.org/9.4/fedora/fedora-19-x86_64/pgdg-fedora94-9.4-1.noarch.rpm'
+default['yum']['postgresql']['9.4']['fedora']['20']['i386']['url'] = 'http://yum.postgresql.org/9.4/fedora/fedora-19-x86_64/pgdg-fedora94-9.4-1.noarch.rpm'
+
+default['yum']['postgresql']['9.3']['fedora']['18']['x86_64']['url'] = 'http://yum.postgresql.org/9.3/fedora/fedora-18-x86_64/pgdg-fedora93-9.3-1.noarch.rpm'
+default['yum']['postgresql']['9.3']['fedora']['18']['i386']['url'] = 'http://yum.postgresql.org/9.3/fedora/fedora-18-x86_64/pgdg-fedora93-9.3-1.noarch.rpm'
+default['yum']['postgresql']['9.3']['fedora']['19']['x86_64']['url'] = 'http://yum.postgresql.org/9.3/fedora/fedora-19-x86_64/pgdg-fedora93-9.3-1.noarch.rpm'
+default['yum']['postgresql']['9.3']['fedora']['19']['i386']['url'] = 'http://yum.postgresql.org/9.3/fedora/fedora-19-x86_64/pgdg-fedora93-9.3-1.noarch.rpm'
+default['yum']['postgresql']['9.3']['fedora']['20']['x86_64']['url'] = 'http://yum.postgresql.org/9.3/fedora/fedora-19-x86_64/pgdg-fedora93-9.3-1.noarch.rpm'
+default['yum']['postgresql']['9.3']['fedora']['20']['i386']['url'] = 'http://yum.postgresql.org/9.3/fedora/fedora-19-x86_64/pgdg-fedora93-9.3-1.noarch.rpm'
+
+default['yum']['postgresql']['9.2']['fedora']['18']['x86_64']['url'] = 'http://yum.postgresql.org/9.2/fedora/fedora-18-x86_64/pgdg-fedora92-9.2-6.noarch.rpm'
+default['yum']['postgresql']['9.2']['fedora']['18']['i386']['url'] = 'http://yum.postgresql.org/9.2/fedora/fedora-18-x86_64/pgdg-fedora92-9.2-6.noarch.rpm'
+default['yum']['postgresql']['9.2']['fedora']['19']['x86_64']['url'] = 'http://yum.postgresql.org/9.2/fedora/fedora-19-x86_64/pgdg-fedora92-9.2-6.noarch.rpm'
+default['yum']['postgresql']['9.2']['fedora']['19']['i386']['url'] = 'http://yum.postgresql.org/9.2/fedora/fedora-19-x86_64/pgdg-fedora92-9.2-6.noarch.rpm'
